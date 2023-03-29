521b202b3a08486ef1a5cc37422a30efed82ad0e
Sébastien Reuiller <sebastien@reuiller.email>
2023-03-29 15:35:29 +0200

    add smtp needed env vars

--- config/settings.py
+++ config/settings.py
@@ -188,3 +188,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "signup.EmailBasedUser"
 AUTHENTICATION_BACKENDS = ["signup.backends.EmailBackend"]
+
+
+# Sending email
+# https://docs.djangoproject.com/en/4.1/topics/email/#smtp-backend
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
