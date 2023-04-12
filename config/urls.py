b7e8a1bc4bcfb211de137037d35918b52005d16e
Sébastien Reuiller <sebastien@reuiller.email>
2023-04-12 17:33:05 +0200

    load debug toolbar only in debug mode

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,9 @@ urlpatterns = [
     path("", include("signup.urls")),
     path("event/", include("event.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG and "debug_toolbar" in settings.INSTALLED_APPS:
+    import debug_toolbar
+
+    urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
